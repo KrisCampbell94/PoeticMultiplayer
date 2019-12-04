552f49a844ac33cdd4f5a164b3785f1e1ec59519
Abby <AbbyNode@users.noreply.github.com>
2019-12-04 12:19:27 -0500

    Split player, camera, gun

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class CameraController : NetworkBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetLocalCamera();
+    }
+    
+    void SetLocalCamera()
+    {
+        if (this.isLocalPlayer)
+        {
+            GameObject oldCamera = GameObject.FindGameObjectWithTag("MainCamera");
+            Camera newCamera = (Camera)Instantiate(oldCamera.GetComponent<Camera>(), transform.position, Quaternion.identity);
+            
+            Destroy(oldCamera);
+
+            //camera_main.SetActive(false);
+            newCamera.enabled = true;
+            newCamera.transform.parent = transform;
+
+            newCamera.transform.localPosition = new Vector3(1.41f, 1, -2.03f);
+            Debug.Log(Vector3.Distance(transform.position, newCamera.transform.position));
+        }
+    }
+}
---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GunController : NetworkBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public GameObject gun;
+    public float bulletSpeed = 30;
+    public float bulletTimer = 10;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.isLocalPlayer)
+        {
+            UpdateGun();
+        }
+    }
+
+    void UpdateGun()
+    {
+        float vRotate = (Input.GetAxis("Mouse Y") * Time.deltaTime * 30) * -1;
+        gun.transform.Rotate(vRotate, 0, 0);
+
+        //if (Input.GetAxis ("Fire1")>0) {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            //Fire ();
+            CmdFire(); //networked fire
+        }
+    }
+
+    [Command]
+    void CmdFire()
+    {
+        var bullet = Instantiate(bulletPrefab, bulletSpawn.position, this.transform.localRotation);
+        if (this.isLocalPlayer)
+        {
+            bullet.GetComponent<MeshRenderer>().material.color = Color.blue;
+        }
+        bullet.GetComponent<Rigidbody>().velocity = gun.transform.up * bulletSpeed;
+        NetworkServer.Spawn(bullet);
+        Destroy(bullet, bulletTimer);
+    }
+}
