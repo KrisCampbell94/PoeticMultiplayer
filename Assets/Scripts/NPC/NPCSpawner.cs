bb26d412a76457bc833745428e17c18448c3e5ec
Kristian Campbell <chipandchuckcreator@gmail.com>
2019-12-09 00:42:20 -0500

    NPCSpawner Fixup

--- Assets/Scripts/NPC/NPCSpawner.cs
+++ Assets/Scripts/NPC/NPCSpawner.cs
@@ -3,20 +3,46 @@ using UnityEngine.Networking;
 
 public class NPCSpawner : NetworkBehaviour
 {
-	public GameObject npcPrefab;
-	public int numOfNpcs;
 
-	private void Start() {
-		if (isServer) {
-			for (int i = 0; i < numOfNpcs; i++) {
-				var spawnPosition = new Vector3(Random.Range(-8.0f, 8.0f), 0.0f, Random.Range(-8.0f, 8.0f));
-				var spawnRotation = Quaternion.Euler(0.0f, Random.Range(0, 180), 0.0f);
-				var npc = (GameObject)Instantiate(npcPrefab, spawnPosition, spawnRotation, this.transform);
-				NetworkServer.Spawn(npc);
-			}
+    public GameObject enemyPrefab;
+    public int numberOfEnemies;
+    private GameObject[] wallObjects;
 
-			GameManager.globalInstance.npcCount += numOfNpcs;
-			GameManager.globalInstance.npcSpawningComplete = true;
-		}
-	}
+    public override void OnStartServer()
+    {
+        wallObjects = GameObject.FindGameObjectsWithTag("Wall");
+
+        for (int i = 0; i < numberOfEnemies; i++)
+        {
+            bool spawnable = true;
+            var spawnPosition = new Vector3(Random.Range(-8.0f, 8.0f), 0.0f, Random.Range(-8.0f, 8.0f));
+            // This checks to see if the spawn position is located within a wall
+            foreach (GameObject wall in wallObjects)
+            {
+                Vector3 size = wall.GetComponent<MeshRenderer>().bounds.size;
+                float distance = Vector3.Distance(spawnPosition, wall.transform.position);
+                // The 4 additive is a double check
+                //      Without it, some spawns will appear inside the wall
+                if (distance <= (size.x + 4) && distance <= (size.z + 4))
+                {
+                    Debug.Log(wall.name + " Contains Enemy " + i);
+                    spawnable = false;
+                    break;
+                }
+            }
+            // If the position is outside the walls, create the spawn
+            if (spawnable)
+            {
+                var spawnRotation = Quaternion.Euler(0.0f, Random.Range(0, 180), 0.0f);
+                var enemy = (GameObject)Instantiate(enemyPrefab, spawnPosition, spawnRotation, this.transform);
+                NetworkServer.Spawn(enemy);
+            }
+            // Try again by deducting the counter by 1
+            else
+            {
+                i -= 1;
+                continue;
+            }
+        }
+    }
 }
